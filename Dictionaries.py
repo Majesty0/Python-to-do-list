e2a19fc11af11ead26487be019aeb117ed0700fb
Kyeremateng Martin <87606847+Majesty0@users.noreply.github.com>
2024-03-13 09:58:50 +0300

    Update Dictionaries.py
    I deleted a task under it which is not related to this code

--- Dictionaries.py
+++ Dictionaries.py
@@ -7,5 +7,3 @@ thisdict = {
 print(thisdict)
 thisdict.update({"year":2023})
 print(thisdict)
-
-"""Write a Python program that asks the user to enter their age. If the age is greater than or equal to 18, print "You are an adult." Otherwise, print "You are a minor."""
